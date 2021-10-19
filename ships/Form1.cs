bf0a1c5ebfefb68ab1e123f4d650d166941ce3a4
usersql <usersql@PK306NEW-3>
2021-10-19 13:25:36 +0500

    Добавьте файлы проекта.

---
+++ ships/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ships
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connect = @"Data Source=PC-306-02;Initial Catalog=Ship;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            string zapros = "select name,class,launched from Ships";
+            using (SqlConnection connection = new SqlConnection(connect))
+            {
+                connection.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(zapros, connect);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    if (Convert.ToInt32(dataGridView1[2, i].Value.ToString()) < 1920)
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Yellow;
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string a = @"Data Source=PC-306-02;Initial Catalog=Ship;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(a);
+            con.Open();
+            DataSet b = new DataSet();
+            String comand = ("SELECT* FROM Classes ");
+            SqlDataAdapter n = new SqlDataAdapter(comand, con);
+            n.Fill(b);
+            dataGridView1.DataSource = b.Tables[0];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string a = @"Data Source=PC-306-02;Initial Catalog=Ship;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(a);
+            con.Open();
+            DataSet b = new DataSet();
+            String comand = ("SELECT* FROM Outcomes ");
+            SqlDataAdapter n = new SqlDataAdapter(comand, con);
+            n.Fill(b);
+            dataGridView1.DataSource = b.Tables[0];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string a = @"Data Source=PC-306-02;Initial Catalog=Ship;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(a);
+            con.Open();
+            DataSet b = new DataSet();
+            String comand = ("SELECT* FROM Battles ");
+            SqlDataAdapter n = new SqlDataAdapter(comand, con);
+            n.Fill(b);
+            dataGridView1.DataSource = b.Tables[0];
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string a = @"Data Source=PC-306-02;Initial Catalog=Ship;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection con = new SqlConnection(a);
+            con.Open();
+            DataSet b = new DataSet();
+            String comand = ("SELECT[Ship], COUNT(ship) * 25 as 'Участие в %' FROM[Outcomes] GROUP BY[Ship] ");
+            SqlDataAdapter n = new SqlDataAdapter(comand, con);
+            n.Fill(b);
+            dataGridView1.DataSource = b.Tables[0];
+        }
+    }
+}
---
+++ ships/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("ships")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ships")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("19971336-7c4b-4a20-85fd-5975274c5b98")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
